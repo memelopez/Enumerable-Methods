e7867779df8bdbb5b8d173dec56565b4d2e9ae35
Elmer Lopez <meme_lopez10@hotmail.com>
2021-06-03 16:43:01 -0600

    Add tests for #my_each_with_index

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,14 +1,24 @@
-# require_relative '../enumerables.rb'
+require_relative '../enumerables'
 
 describe 'Enumerables' do
   let(:numeric_arr) { [1, 2, 3, 4] }
 
   describe '#my_each' do
-    it 'simulates the normal #each in ruby' do
+    it 'simulates normal #each in ruby' do
       expect(numeric_arr.my_each { |el| (el * 5) }).to eq(numeric_arr.each { |i| i.send('*', 5) })
     end
 
-    it 'returns enumerator when no block is given' do
+    it 'returns enumerator when block is not given' do
+      expect(numeric_arr.my_each).to be_an(Enumerator)
+    end
+  end
+
+  describe '#my_each_with_index' do
+    it 'simulates normal each in ruby with index' do
+      expect(numeric_arr.my_each_with_index { |el, i| el + i }).to eq(numeric_arr.each_with_index { |el, i| el + i })
+    end
+
+    it 'returns enumerator when block is not given' do
       expect(numeric_arr.my_each).to be_an(Enumerator)
     end
   end
