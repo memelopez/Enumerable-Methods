de8386519803fd0ef6d4816265515af2c5e2c10d
Elmer Lopez <meme_lopez10@hotmail.com>
2021-06-04 17:09:29 -0600

    Add tests for #my_none? method

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -4,6 +4,7 @@ describe 'Enumerables' do
   let(:numeric_arr) { [1, 2, 3, 4] }
   let(:mix_arr) { ['Skip', 100, 'Shanon', -100] }
   let(:str_arr) { ['Micheal Jordan', 'Lebron', 'Ja Morant', 'Luka Dončič', 'Devin Booker'] }
+  nil_arr = [nil, nil, false]
   let(:is_int) { proc { |int| int.is_a? Integer } }
 
   describe '#my_each' do
@@ -104,7 +105,6 @@ describe 'Enumerables' do
       end
 
       it 'returns false only if none of the elements in the array evaluate to true' do
-        nil_arr = [nil, nil, false]
         expect(nil_arr.my_any?).to be nil_arr.any?
       end
     end
@@ -117,6 +117,42 @@ describe 'Enumerables' do
       it 'returns false only if none of the elements in the array mathces the regex expression' do
         expect(str_arr.my_any?(/!/)).to be_falsey
       end
+    end
+  end
+
+  describe '#my_none?' do
+    it 'returns if none of the elements in the array evaluate to true' do
+      expect(str_arr.my_none?(&is_int)).to eq(str_arr.none?(&is_int))
+    end
+
+    it 'returns false if at least one of the elements in the array evaluates to true' do
+      expect(mix_arr.my_none?(&is_int)).to eq(mix_arr.none?(&is_int))
+    end
+
+    context 'when no block or argument is given' do
+      it 'returns true if none of the elements evaluate to true' do
+        expect(nil_arr.my_none?).to be nil_arr.none?
+      end
+
+      it 'returns false if at least one of the elements in the array evaluates to true' do
+        expect(numeric_arr.my_none?).to be numeric_arr.none?
+      end
+    end
+
+    context 'when a regex is passed as an argument' do
+      it 'returns true if none of the elements in the array match the regular expression' do
+        expect(str_arr.my_none?(/!/)).to be_truthy
+      end
+
+      it 'returns false if at least one of the elements match the regular expression' do
+        expect(str_arr.my_none?(/M/)).to be_falsey
+      end
+    end
+  end
+
+  describe '#my_count' do
+    it 'returns the number of elements that are equal to the argument' do
+      expect(numeric_arr.my_count(2)).to eq(numeric_arr.count(2))
     end
   end
 end
